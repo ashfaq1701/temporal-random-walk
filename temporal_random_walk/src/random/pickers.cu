02275d25e93d394fbf67109d7e58805fd25baa2a
Ashfaq Salehin <ashfaq.aws@gmail.com>
2025-04-15 02:47:18 +0100

    Passed random picker from outside

--- temporal_random_walk/src/random/pickers.cu
+++ temporal_random_walk/src/random/pickers.cu
@@ -9,7 +9,7 @@
 
 #include "../utils/random.cuh"
 
-HOST int random_pickers::pick_random_linear_host(const int start, const int end, const bool prioritize_end) {
+HOST DEVICE int random_pickers::pick_random_linear(const int start, const int end, const bool prioritize_end, const double rand_number) {
     if (start >= end) {
         throw std::invalid_argument("Start must be less than end.");
     }
@@ -24,12 +24,12 @@ HOST int random_pickers::pick_random_linear_host(const int start, const int end,
                                    (static_cast<double>(len_seq) + 1.0) / 2.0;
 
     // Generate random value in [0, total_weight)
-    const double random_value = generate_random_value_host(0.0, total_weight);
+    const double scaled_random_value = rand_number * rand_number;
 
     // For both cases, we solve quadratic equation i² + i - 2r = 0
     // where r is our random value (or transformed random value)
     // Using quadratic formula: (-1 ± √(1 + 8r))/2
-    const double discriminant = 1.0 + 8.0 * random_value;
+    const double discriminant = 1.0 + 8.0 * scaled_random_value;
     const double root = (-1.0 + std::sqrt(discriminant)) / 2.0;
     const int index = static_cast<int>(std::floor(root));
 
@@ -44,25 +44,22 @@ HOST int random_pickers::pick_random_linear_host(const int start, const int end,
     }
 }
 
-HOST int random_pickers::pick_random_exponential_index_host(const int start, const int end, const bool prioritize_end) {
+HOST DEVICE int random_pickers::pick_random_exponential_index(const int start, const int end, const bool prioritize_end, const double rand_number) {
     if (start >= end) {
         throw std::invalid_argument("Start must be less than end.");
     }
 
     const int len_seq = end - start;
 
-    // Generate uniform random number between 0 and 1
-    const double u = generate_random_value_host(0.0, 1.0);
-
     double k;
     if (len_seq < 710) {
         // Inverse CDF formula,
         // k = ln(1 + u * (e^len seq − 1)) − 1
-        k = log1p(u * expm1(len_seq)) - 1;
+        k = log1p(rand_number * expm1(len_seq)) - 1;
     } else {
         // Inverse CDF approximation for large len_seq,
         // k = len_seq + ln(u) − 1
-        k = len_seq + std::log(u) - 1;
+        k = len_seq + std::log(rand_number) - 1;
     }
 
     // Due to rounding, the trailing "-1" in the inverse CDF formula causes error.
@@ -77,15 +74,15 @@ HOST int random_pickers::pick_random_exponential_index_host(const int start, con
     }
 }
 
-HOST int random_pickers::pick_random_uniform_host(int start, int end) {
+HOST DEVICE int random_pickers::pick_random_uniform(const int start, const int end, const double rand_number) {
     if (start >= end) {
         throw std::invalid_argument("Start must be less than end.");
     }
 
-    return generate_random_int_host(start, end - 1);
+    return start + static_cast<int>(rand_number * (end - start));
 }
 
-HOST int random_pickers::pick_random_exponential_weights_host(double* weights, const size_t weights_size, const size_t group_start, const size_t group_end) {
+HOST int random_pickers::pick_random_exponential_weights_host(double* weights, const size_t weights_size, const size_t group_start, const size_t group_end, const double random_number) {
     if (group_start >= group_end || group_end > weights_size) {
         return -1;
     }
@@ -101,8 +98,8 @@ HOST int random_pickers::pick_random_exponential_weights_host(double* weights, c
         return -1;
     }
 
-    // Generate random value between [start_sum, end_sum]
-    const double random_val = generate_random_value_host(start_sum, end_sum);
+    const double random_val = start_sum + random_number * (end_sum - start_sum);
+
     return static_cast<int>(std::lower_bound(
             weights + group_start,
             weights + group_end,
@@ -110,83 +107,8 @@ HOST int random_pickers::pick_random_exponential_weights_host(double* weights, c
 }
 
 #ifdef HAS_CUDA
-DEVICE int random_pickers::pick_random_linear_device(const int start, const int end, const bool prioritize_end, curandState* rand_state) {
-    if (start >= end) {
-        return -1;
-    }
 
-    const int len_seq = end - start;
-
-    // For a sequence of length n, weights form an arithmetic sequence
-    // When prioritizing end: weights are 1, 2, 3, ..., n
-    // When prioritizing start: weights are n, n-1, n-2, ..., 1
-    // Sum of arithmetic sequence = n(a1 + an)/2 = n(n+1)/2
-    const double total_weight = static_cast<double>(len_seq) *
-                                   (static_cast<double>(len_seq) + 1.0) / 2.0;
-
-    // Generate random value in [0, total_weight)
-    const double random_value = generate_random_value_device(0.0, total_weight, rand_state);
-
-    // For both cases, we solve quadratic equation i² + i - 2r = 0
-    // where r is our random value (or transformed random value)
-    // Using quadratic formula: (-1 ± √(1 + 8r))/2
-    const double discriminant = 1.0 + 8.0 * random_value;
-    const double root = (-1.0 + std::sqrt(discriminant)) / 2.0;
-    const int index = static_cast<int>(std::floor(root));
-
-    if (prioritize_end) {
-        // For prioritize_end=true, larger indices should have higher probability
-        return start + std::min(index, len_seq - 1);
-    } else {
-        // For prioritize_end=false, we reverse the index to give
-        // higher probability to smaller indices
-        const int revered_index = len_seq - 1 - index;
-        return start + std::max(0, revered_index);
-    }
-}
-
-DEVICE int random_pickers::pick_random_exponential_index_device(const int start, const int end, const bool prioritize_end, curandState* rand_state) {
-    if (start >= end) {
-        return -1;
-    }
-
-    const int len_seq = end - start;
-
-    // Generate uniform random number between 0 and 1
-    const double u = generate_random_value_device(0.0, 1.0, rand_state);
-
-    double k;
-    if (len_seq < 710) {
-        // Inverse CDF formula,
-        // k = ln(1 + u * (e^len seq − 1)) − 1
-        k = log1p(u * expm1(len_seq)) - 1;
-    } else {
-        // Inverse CDF approximation for large len_seq,
-        // k = len_seq + ln(u) − 1
-        k = len_seq + std::log(u) - 1;
-    }
-
-    // Due to rounding, the trailing "-1" in the inverse CDF formula causes error.
-    // To compensate for this we add 1 with k.
-    // And bound the results within limits.
-    const int rounded_index = std::max(0, std::min(static_cast<int>(k + 1), len_seq - 1));
-
-    if (prioritize_end) {
-        return start + rounded_index;
-    } else {
-        return start + (len_seq - 1 - rounded_index);
-    }
-}
-
-DEVICE int random_pickers::pick_random_uniform_device(const int start, const int end, curandState* rand_state) {
-    if (start >= end) {
-        return -1;
-    }
-
-    return generate_random_int_device(start, end - 1, rand_state);
-}
-
-DEVICE int random_pickers::pick_random_exponential_weights_device(double* weights, const size_t weights_size, const size_t group_start, const size_t group_end, curandState* rand_state) {
+DEVICE int random_pickers::pick_random_exponential_weights_device(double* weights, const size_t weights_size, const size_t group_start, const size_t group_end, const double random_number) {
     if (group_start >= group_end || group_end > weights_size) {
         return -1;
     }
@@ -202,13 +124,14 @@ DEVICE int random_pickers::pick_random_exponential_weights_device(double* weight
         return -1;
     }
 
-    // Generate random value between [start_sum, end_sum]
-    const double random_val = generate_random_value_device(start_sum, end_sum, rand_state);
+    const double random_val = start_sum + random_number * (end_sum - start_sum);
+
     return static_cast<int>(cuda::std::lower_bound(
             weights + group_start,
             weights + group_end,
             random_val) - weights);
 }
+
 
 #endif
 
@@ -219,14 +142,14 @@ HOST DEVICE bool random_pickers::is_index_based_picker(const RandomPickerType pi
             picker_type == RandomPickerType::TEST_FIRST || picker_type == RandomPickerType::TEST_LAST;
 }
 
-HOST int random_pickers::pick_using_index_based_picker_host(const RandomPickerType random_picker, const int start, const int end, const bool prioritize_end) {
+HOST DEVICE int random_pickers::pick_using_index_based_picker(const RandomPickerType random_picker, const int start, const int end, const bool prioritize_end, const double random_number) {
     switch (random_picker) {
         case RandomPickerType::Linear:
-            return pick_random_linear_host(start, end, prioritize_end);
+            return pick_random_linear(start, end, prioritize_end, random_number);
         case RandomPickerType::ExponentialIndex:
-            return pick_random_exponential_index_host(start, end, prioritize_end);
+            return pick_random_exponential_index(start, end, prioritize_end, random_number);
         case RandomPickerType::Uniform:
-            return pick_random_uniform_host(start, end);
+            return pick_random_uniform(start, end, random_number);
         // ONLY FOR TEST
         case RandomPickerType::TEST_FIRST:
             return start;
@@ -237,40 +160,22 @@ HOST int random_pickers::pick_using_index_based_picker_host(const RandomPickerTy
     }
 }
 
-HOST int random_pickers::pick_using_weight_based_picker_host(const RandomPickerType random_picker, double* weights, const size_t weights_size, const size_t group_start, const size_t group_end) {
+HOST int random_pickers::pick_using_weight_based_picker_host(const RandomPickerType random_picker, double* weights, const size_t weights_size, const size_t group_start, const size_t group_end, const double random_number) {
     if (random_picker != RandomPickerType::ExponentialWeight) {
         return -1;
     }
 
-    return pick_random_exponential_weights_host(weights, weights_size, group_start, group_end);
+    return pick_random_exponential_weights_host(weights, weights_size, group_start, group_end, random_number);
 }
 
 #ifdef HAS_CUDA
 
-DEVICE int random_pickers::pick_using_index_based_picker_device(const RandomPickerType random_picker, const int start, const int end, const bool prioritize_end, curandState* rand_state) {
-    switch (random_picker) {
-    case RandomPickerType::Linear:
-        return pick_random_linear_device(start, end, prioritize_end, rand_state);
-    case RandomPickerType::ExponentialIndex:
-        return pick_random_exponential_index_device(start, end, prioritize_end, rand_state);
-    case RandomPickerType::Uniform:
-        return pick_random_uniform_device(start, end, rand_state);
-        // ONLY FOR TEST
-    case RandomPickerType::TEST_FIRST:
-        return start;
-    case RandomPickerType::TEST_LAST:
-        return end - 1;
-    default:
-        return -1;
-    }
-}
-
-DEVICE int random_pickers::pick_using_weight_based_picker_device(const RandomPickerType random_picker, double* weights, const size_t weights_size, const size_t group_start, const size_t group_end, curandState* rand_state) {
+DEVICE int random_pickers::pick_using_weight_based_picker_device(const RandomPickerType random_picker, double* weights, const size_t weights_size, const size_t group_start, const size_t group_end, const double random_number) {
     if (random_picker != RandomPickerType::ExponentialWeight) {
         return -1;
     }
 
-    return pick_random_exponential_weights_device(weights, weights_size, group_start, group_end, rand_state);
+    return pick_random_exponential_weights_device(weights, weights_size, group_start, group_end, random_number);
 }
 
 #endif
--- temporal_random_walk/src/random/pickers.cuh
+++ temporal_random_walk/src/random/pickers.cuh
@@ -10,37 +10,29 @@
 
 namespace random_pickers {
 
-    HOST int pick_random_linear_host(int start, int end, bool prioritize_end);
+    HOST DEVICE int pick_random_linear(int start, int end, bool prioritize_end, double rand_number);
 
-    HOST int pick_random_exponential_index_host(int start, int end, bool prioritize_end);
+    HOST DEVICE int pick_random_exponential_index(int start, int end, bool prioritize_end, double rand_number);
 
-    HOST int pick_random_uniform_host(int start, int end);
+    HOST int pick_random_uniform(int start, int end, double rand_number);
 
-    HOST int pick_random_exponential_weights_host(double* weights, size_t weights_size, size_t group_start, size_t group_end);
+    HOST int pick_random_exponential_weights_host(double* weights, size_t weights_size, size_t group_start, size_t group_end, double rand_number);
 
     #ifdef HAS_CUDA
 
-    DEVICE int pick_random_linear_device(int start, int end, bool prioritize_end, curandState* rand_state);
-
-    DEVICE int pick_random_exponential_index_device(int start, int end, bool prioritize_end, curandState* rand_state);
-
-    DEVICE int pick_random_uniform_device(int start, int end, curandState* rand_state);
-
-    DEVICE int pick_random_exponential_weights_device(double* weights, size_t weights_size, size_t group_start, size_t group_end, curandState* rand_state);
+    DEVICE int pick_random_exponential_weights_device(double* weights, size_t weights_size, size_t group_start, size_t group_end, double rand_number);
 
     #endif
 
     HOST DEVICE bool is_index_based_picker(RandomPickerType picker_type);
 
-    HOST int pick_using_index_based_picker_host(RandomPickerType random_picker, int start, int end, bool prioritize_end);
+    HOST DEVICE int pick_using_index_based_picker(RandomPickerType random_picker, int start, int end, bool prioritize_end, double rand_number);
 
-    HOST int pick_using_weight_based_picker_host(RandomPickerType random_picker, double* weights, size_t weights_size, size_t group_start, size_t group_end);
+    HOST int pick_using_weight_based_picker_host(RandomPickerType random_picker, double* weights, size_t weights_size, size_t group_start, size_t group_end, double rand_number);
 
     #ifdef HAS_CUDA
 
-    DEVICE int pick_using_index_based_picker_device(RandomPickerType random_picker, int start, int end, bool prioritize_end, curandState* rand_state);
-
-    DEVICE int pick_using_weight_based_picker_device(RandomPickerType random_picker, double* weights, size_t weights_size, size_t group_start, size_t group_end, curandState* rand_state);
+    DEVICE int pick_using_weight_based_picker_device(RandomPickerType random_picker, double* weights, size_t weights_size, size_t group_start, size_t group_end, double rand_number);
 
     #endif
 }
