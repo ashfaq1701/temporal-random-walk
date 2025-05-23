dba2bbe9eb3b668765bb7272d98f6f3f3481ec0d
Ashfaq Salehin <ashfaq.aws@gmail.com>
2025-05-23 20:15:03 +0100

    Reverted binary search

--- temporal_random_walk/src/stores/node_edge_index.cu
+++ temporal_random_walk/src/stores/node_edge_index.cu
@@ -4,6 +4,7 @@
 #include <thrust/device_ptr.h>
 #include <thrust/iterator/counting_iterator.h>
 #include <thrust/sequence.h>
+#include <thrust/binary_search.h>
 #include "../common/cuda_sort.cuh"
 #endif
 
@@ -1227,7 +1228,7 @@ HOST void node_edge_index::update_temporal_weights_cuda(
     double timescale_bound
 ) {
     // Get the number of nodes and timestamp groups
-    const size_t node_index_capacity = node_edge_index->outbound_offsets_size - 1;
+    size_t node_index_capacity = node_edge_index->outbound_offsets_size - 1;
     const size_t outbound_groups_size = node_edge_index->outbound_timestamp_group_indices_size;
 
     // Resize outbound weight arrays
@@ -1267,18 +1268,20 @@ HOST void node_edge_index::update_temporal_weights_cuda(
 
         size_t* outbound_offsets_ptr = outbound_offsets.data;
 
-        thrust::for_each(
+        thrust::upper_bound(
             DEVICE_EXECUTION_POLICY,
+            outbound_offsets_ptr,
+            outbound_offsets_ptr + node_index_capacity + 1,
             thrust::make_counting_iterator<size_t>(0),
-            thrust::make_counting_iterator<size_t>(node_index_capacity),
-            [outbound_offsets_ptr, group_to_node_ptr] DEVICE(const size_t node) {
-                const size_t out_start = outbound_offsets_ptr[node];
-                const size_t out_end = outbound_offsets_ptr[node + 1];
+            thrust::make_counting_iterator<size_t>(outbound_groups_size),
+            group_to_node.begin()
+        );
 
-                for (size_t pos = out_start; pos < out_end; ++pos) {
-                    group_to_node_ptr[pos] = node;
-                }
-            }
+        thrust::transform(
+            DEVICE_EXECUTION_POLICY,
+            group_to_node.begin(), group_to_node.end(),
+            group_to_node.begin(),
+            [] DEVICE(const size_t x) { return x - 1; }
         );
 
         // Step 2: Compute min/max timestamps per node
@@ -1424,6 +1427,8 @@ HOST void node_edge_index::update_temporal_weights_cuda(
 
     // Process inbound weights (only backward)
     if (node_edge_index->inbound_offsets_size > 0) {
+        node_index_capacity = node_edge_index->inbound_offsets_size - 1;
+
         MemoryView<size_t> inbound_offsets = get_timestamp_offset_vector(node_edge_index, false, true);
         const size_t inbound_groups_size = node_edge_index->inbound_timestamp_group_indices_size;
 
@@ -1432,6 +1437,22 @@ HOST void node_edge_index::update_temporal_weights_cuda(
         auto group_to_node_ptr = thrust::raw_pointer_cast(group_to_node.data());
 
         size_t* inbound_offsets_ptr = inbound_offsets.data;
+
+        thrust::upper_bound(
+            DEVICE_EXECUTION_POLICY,
+            inbound_offsets_ptr,
+            inbound_offsets_ptr + node_index_capacity + 1,
+            thrust::make_counting_iterator<size_t>(0),
+            thrust::make_counting_iterator<size_t>(inbound_groups_size),
+            group_to_node.begin()
+        );
+
+        thrust::transform(
+            DEVICE_EXECUTION_POLICY,
+            group_to_node.begin(), group_to_node.end(),
+            group_to_node.begin(),
+            [] DEVICE(const size_t x) { return x - 1; }
+        );
 
         thrust::for_each(
             DEVICE_EXECUTION_POLICY,
