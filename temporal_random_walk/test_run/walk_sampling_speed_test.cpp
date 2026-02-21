5071ccece6bd468bd11d04b9fd700587940b6c7b
Ashfaq Salehin <ashfaq.salehin1701@gmail.com>
2026-02-21 03:02:46 +0000

    Add pregen random script

--- temporal_random_walk/test_run/walk_sampling_speed_test.cpp
+++ temporal_random_walk/test_run/walk_sampling_speed_test.cpp
@@ -153,7 +153,7 @@ int main(int argc, char* argv[])
     // ============================
 
     bool enable_temporal_node2vec = edge_picker_enum == RandomPickerType::TemporalNode2Vec;
-    bool enable_weight_computation = edge_picker_enum == RandomPickerType::ExponentialWeight || RandomPickerType::ExponentialWeight;
+    bool enable_weight_computation = edge_picker_enum == RandomPickerType::ExponentialWeight || start_picker_enum == RandomPickerType::ExponentialWeight;
 
     KernelLaunchType kernel_launch_type = kernel_launch_type_str == "FULL_WALK" ? KernelLaunchType::FULL_WALK : KernelLaunchType::STEP_BASED;
 
